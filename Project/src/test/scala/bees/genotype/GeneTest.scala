eaaebd9ede2914b502a89b271a71bdb5d15d055d
scumatteo <matteo.scucchia@studio.unibo.it>
2020-08-12 09:57:39 +0200

    Added test for Influence, Gene and GeneTaxonomy. Work group, pair programming: Fantoni - Scucchia

--- Project/src/test/scala/bees/genotype/GeneTest.scala
+++ Project/src/test/scala/bees/genotype/GeneTest.scala
@@ -3,22 +3,25 @@ package bees.genotype
 import bees.genotype.Gene._
 import org.scalatest.funsuite.AnyFunSuite
 
-class GeneTest extends AnyFunSuite{
+class GeneTest extends AnyFunSuite {
   val temperatureGene: Gene = Gene(Some(GeneTaxonomy.TEMPERATURE_GENE))
   val genericGene: Gene = Gene()
 
-  test("A Gene should have a name"){
+  test("A Gene should have a name") {
     assert(temperatureGene.name != null)
   }
 
-  test("A Gene should have at least one information"){
+  test("A GeneTaxonomy should raise a NoSuchElementException if doesn't belong to GeneTaxonomy") {
+    assertThrows[NoSuchElementException](GeneTaxonomy(GeneTaxonomy.maxId + 1))
+  }
+
+  test("A Gene should have at least one information") {
     assert(temperatureGene.information.getCharacteristics.nonEmpty)
   }
 
-  test("A generic Gene should have a valid name"){
+  test("A generic Gene should have a valid name") {
     assert(GeneTaxonomy.values.contains(genericGene.name))
   }
-
 
 
 }
---
+++ Project/src/test/scala/bees/genotype/InfluenceTest.scala
@@ -0,0 +1,25 @@
+package bees.genotype
+
+import bees.genotype.Influence.{Influence, InfluenceImpl}
+import org.scalatest.funsuite.AnyFunSuite
+
+class InfluenceTest extends AnyFunSuite{
+  val influence: Influence = InfluenceImpl()
+  val negativeInfluence: Influence = InfluenceImpl(typeOfInfluence = InfluenceType.NEGATIVE)
+
+  test("An Influence should have default parameters"){
+    assert(influence.influenceValue == 1.0 && influence.typeOfInfluence.equals(InfluenceType.POSITIVE))
+  }
+
+  test("An Influence should raise IllegalArgumentException if % doesn't belong to range (0, 100)"){
+    assertThrows[IllegalArgumentException](InfluenceImpl(influenceInPercentage = -1))
+  }
+
+  test("An Influence should raise NoSuchElementException if influence doesn't belong to InfluenceType"){
+    assertThrows[NoSuchElementException](InfluenceType(InfluenceType.maxId+1))
+  }
+
+  test("An Influence should return a negative InfluenceValue if InfluenceType is negative"){
+    assert(negativeInfluence.influenceValue <= 0)
+  }
+}

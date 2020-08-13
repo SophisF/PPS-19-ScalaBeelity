ffd911ecdf58d596a0be3d8974b203519c05439a
Paolo Baldini <paolobaldini01@gmail.com>
2020-08-13 13:44:30 +0200

    added limit check on cell properties

--- Project/src/main/scala/model/Cell.scala
+++ Project/src/main/scala/model/Cell.scala
@@ -1,16 +1,15 @@
 package scala.model
 
-import scala.model.property.Property.{Property, Temperature, Humidity, Pressure}
-import scala.model.property.PropertyVariation
-import scala.model.property.PropertyVariation._
+import scala.model.Cell.limit
+import scala.model.property.Property.{Humidity, Pressure, Property, Temperature}
+import scala.model.property.{Property, PropertyVariation}
 
 /**
  * Class that represent an environment cell
- * TODO study how to develop it
  *
- * @param temperature of the cell
- * @param humidity of the cell
- * @param pressure of the cell
+ * @param temperature in the area represented by the cell
+ * @param humidity in the area represented by the cell
+ * @param pressure in the area represented by the cell
  *
  * @author Paolo Baldini
  */
@@ -28,11 +27,6 @@ case class Cell(temperature: Int, humidity: Int, pressure: Int) {
     case Pressure => pressure
   }
 
-  def +(variation: Option[PropertyVariation]): Cell = variation match {
-    case None => this
-    case _ => this + variation.get
-  }
-
   /**
    * Variate a specific property of the cell summing the value specified by the variation
    *
@@ -40,8 +34,45 @@ case class Cell(temperature: Int, humidity: Int, pressure: Int) {
    * @return the varied cell
    */
   def +(variation: PropertyVariation): Cell = variation.property match {
-    case Temperature => Cell(temperature + variation.value, humidity, pressure)
-    case Humidity => Cell(temperature, humidity + variation.value, pressure)
-    case Pressure => Cell(temperature, humidity, pressure + variation.value)
+    case Temperature => Cell(limit(Temperature, temperature + variation.value), humidity, pressure)
+    case Humidity => Cell(temperature, limit(Humidity, humidity + variation.value), pressure)
+    case Pressure => Cell(temperature, humidity, limit(Pressure, pressure + variation.value))
+  }
+
+  /**
+   * Make an optional sum. If the optional is empty, it returns the same cell;
+   * otherwise exec the normal sum
+   *
+   * @param variation optionally contains value and target of the variation
+   * @return the optionally varied cell
+   */
+  def +(variation: Option[PropertyVariation]): Cell = variation map(this + _) getOrElse this
+}
+object Cell {
+
+  /**
+   * Apply field-wise operation between two cell. Resulting value cannot exceed property limits/range
+   *
+   * @param first a cell
+   * @param second a cell
+   * @param property the operation between element of 'first' and 'second'
+   * @return a cell on which every field is the (limited) result of the operation applied to between the value of the
+   *        'first' and 'second' cells
+   */
+  def operation(first: Cell, second: Cell)(property: (Int, Int) => Int): Cell = Cell(
+    limit(Temperature, property(first.temperature, second.temperature)),
+    limit(Humidity, property(first.humidity, second.humidity)),
+    limit(Pressure, property(first.pressure, second.pressure)))
+
+  /**
+   * Make the passed value respect the limit decreed by the property
+   *
+   * @param property of which use the range
+   * @param value to set in range
+   * @return ranged value
+   */
+  private def limit(property: Property, value: Int): Int = value match {
+    case value if value >= 0 => Math.min(value, Property.range(property).maxValue)
+    case value => Math.max(value, Property.range(property).minValue)
   }
 }

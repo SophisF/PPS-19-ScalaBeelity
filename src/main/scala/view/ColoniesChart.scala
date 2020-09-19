685a46363ae1d548b0677dd78bf6f7ec25e070ea
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2020-09-19 21:02:23 +0200

    Add statistics on colnieschart.

--- src/main/scala/view/ColoniesChart.scala
+++ src/main/scala/view/ColoniesChart.scala
@@ -1,14 +1,23 @@
 package scala.view
 
-import java.awt.{BorderLayout, Color, Component}
+import java.awt.{BorderLayout, Color, Component, GridLayout}
 
-import javax.swing.{JLabel, JPanel}
+import javax.swing.{JLabel, JPanel, JScrollPane}
 
 import scala.controller.Controller
 import scala.utility.Point
 
 
 class ColoniesChart[T <: Seq[(Point, Int)]](controller: Controller) extends Chart[T] {
+
+  def createStatistical(panel: JPanel, title: String, statistics: List[Int]) = {
+    panel.add(new JLabel(title + "        "))
+    statistics.zip(controller.color).foreach(d => {
+      val l = new JLabel(d._1.toString)
+      l.setForeground(Color.getHSBColor(d._2.toFloat, 1, 1))
+      panel.add(l)
+    })
+  }
 
 
   override def createChart(data: T): Component = {
@@ -22,16 +31,18 @@ class ColoniesChart[T <: Seq[(Point, Int)]](controller: Controller) extends Char
 
     //TODO: da provare
 
+    val scrol = new JScrollPane()
     val label = new JPanel()
+    label.setLayout(new GridLayout(4, data.size))
 
 
-    label.add(new JLabel("Aggresivity"))
-    controller.statisticalData.averageAggression.zip(controller.color).foreach(d => {
-      val l = new JLabel(d._1.toString)
-      l.setForeground(Color.getHSBColor(d._2.toFloat, 1, 1))
-      label.add(l)
-    })
-    panel.add(label, BorderLayout.EAST)
+    createStatistical(label, "Aggr",controller.statisticalData.averageAggression )
+    createStatistical(label, "Long",controller.statisticalData.averageLongevity )
+    createStatistical(label, "Repr",controller.statisticalData.averageReproduction )
+    createStatistical(label, "Speed",controller.statisticalData.averageSpeed )
+    scrol.setSize(300,300)
+    scrol.add(label)
+    panel.add(scrol, BorderLayout.EAST)
 
     val colonies = new ColoniesComponentPanel(controller, data zip controller.color)
     colonies.setSize(500, 500)

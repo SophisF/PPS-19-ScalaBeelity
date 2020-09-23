108141c5518fa2165bfa6c555707a8a17e98d5b2
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2020-09-23 11:10:59 +0200

    Correct print of info for colonies in chart

--- src/main/scala/view/ColoniesChartBuilder.scala
+++ src/main/scala/view/ColoniesChartBuilder.scala
@@ -67,8 +67,8 @@ object ColoniesChartBuilder extends ChartBuilder[StatisticColony] {
     new Rectangle2D.Double(point x, point y, size width, size height), new BasicStroke(2f), color, color)
 
   private def colonyToString(colonyChar: (Colony, Set[(CharacteristicTaxonomy.Value, Characteristic#Expression)])): String = {
-    var str : String = s"Colony info: \n Position: ${colonyChar._1.center x} ${colonyChar._1.center y}\nBees' number: ${colonyChar._1.bees size}"
-    colonyChar._2.foreach(t => str = str.+(s"\n ${t._1}, ${t._2}".toString))
+    var str = s"Colony info: \nPosition: ${colonyChar._1.center x} ${colonyChar._1.center y}\nBees' number: ${colonyChar._1.bees size}"
+    colonyChar._2.foreach(t => str = str.+(s"\n${t._1}, ${t._2}"))
     str
   }
 

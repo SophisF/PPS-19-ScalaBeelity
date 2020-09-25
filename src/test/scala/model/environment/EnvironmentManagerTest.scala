8e86eb2fefb5f84d711fed894aa1a0b787547d15
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2020-09-25 11:54:30 +0200

    Refactoring IntRange.

--- src/test/scala/model/environment/EnvironmentManagerTest.scala
+++ src/test/scala/model/environment/EnvironmentManagerTest.scala
@@ -1,5 +1,31 @@
 package scala.model.environment
 
-class EnvironmentManagerTest extends {
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.model.Time
+import scala.model.environment.EnvironmentManager.evolution
+
+class EnvironmentManagerTest extends AnyFunSuite {
+
+  private val E = "Environment Manager"
+
+  test(s"$E should change after 5 iterations.") {
+    Time.reset()
+    val env2 = EnvironmentManager(30, 30)
+    Time.increment(10)
+    assert(!(env2.environment.map equals evolution(env2).environment.map))
+  }
+
+  test(s"$E should change after 10 iteration.") {
+    Time.reset()
+    val env = EnvironmentManager(20, 20)
+    Time.increment(5)
+    assert(!(env.environment.map equals evolution(env).environment.map))
+  }
+
+  test(s"$E should give cells matrix, whose represent environment matrix. ") {
+    val environment = EnvironmentManager(30, 30)
+    assert(environment.environment.map equals environment.cells().map(_.asInstanceOf[Cell]))
+  }
 
 }

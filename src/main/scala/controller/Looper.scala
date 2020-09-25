bbb8da5d49e8b374fd2bb66fc68caef1bace910c
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2020-09-25 17:16:47 +0200

    Remove some TODO.

--- src/main/scala/controller/Looper.scala
+++ src/main/scala/controller/Looper.scala
@@ -8,33 +8,6 @@ import scala.annotation.tailrec
  * @author Paolo Baldini
  */
 object Looper {
-
-  /*
-    /**
-     * Run a simulation
-     *
-     * @param environmentSize size of the map
-     * @param iterations      number of iterations to simulate
-     * @param updateStep      how often update the environment
-     */
-    def run(environmentSize: (Int, Int), iterations: Int, updateStep: Int): Unit = {
-      var environmentManager = EnvironmentManager(environmentSize._1, environmentSize._2)
-      val statisticalData = StatisticalData(Time.time)
-      createAndShowGUI(statisticalData, Time.time)
-
-
-      //plot(environmentManager.environment)
-
-
-      environmentManager = GeneratorClimateChange.generateClimate(environmentSize._1, environmentSize._2, iterations)
-        .foldLeft(environmentManager)(addSource)
-
-      environmentManager = GeneratorClimateChange.generateSeason().foldLeft(environmentManager)(addSource)
-
-
-   */
-
-  //TODO: Da sostituire con Ecosystem
   @tailrec
   def loop(iterations: Int)(updateModel: () => Unit)(updateView: () => Unit): Unit = iterations match {
     case 0 =>
@@ -43,22 +16,4 @@ object Looper {
       updateView()
       loop(iterations - 1)(updateModel)(updateView)
   }
-
-
-  //loop(iterations)(update)
-  //}
-
-  /* /**
-  * Plot the environment calling the view
-  *
-  * @param environment to plot
-  */
- private def plot(environment: Environment): Unit = Property.values.foreach(property => BreezeView.plot(
-   environment.map.dropColumns(0.5).dropRows(0.5).mapValues(c => toPercentage(property, c get property) toDouble),
-   0,
-   100,
-   s"${property.toString} (${Property.range(property).minValue}, ${Property.range(property).maxValue})"
- ))*/
-
-  //}
 }

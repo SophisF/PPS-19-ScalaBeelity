abba1dd1faac092270e1cf0074e80d2c50bdd378
Paolo Baldini <paolobaldini01@gmail.com>
2020-09-26 12:10:36 +0200

    added docs and moved some utilities in different file

---
+++ src/main/scala/utility/Conversion.scala
@@ -0,0 +1,16 @@
+package scala.utility
+
+object Conversion {
+
+  implicit def iteratorOf[A](iterable: Iterable[A]): Iterator[A] = iterable iterator
+
+  implicit def iterableOf[A](iterator: Iterator[A]): Iterable[A] = iterator to Iterable
+
+  implicit def mapOf[K, V](iterable: Iterable[(K, V)]): Map[K, V] = iterable toMap
+
+  implicit def mapOf[K, V](iterator: Iterator[(K, V)]): Map[K, V] = iterator toMap
+
+  implicit def sequenceOf[A](iterable: Iterable[A]): Seq[A] = iterable toSeq
+
+  implicit def sequenceOf[A](iterator: Iterator[A]): Seq[A] = iterator toSeq
+}
--- src/main/scala/utility/IterableHelper.scala
+++ src/main/scala/utility/IterableHelper.scala
@@ -1,8 +1,8 @@
 package scala.utility
 
 import scala.util.Random
-
 import Tuple.RichTuple
+import scala.utility.Conversion.{iteratorOf, sequenceOf}
 
 /**
  * Add utility methods to Iterable
@@ -17,21 +17,23 @@ object IterableHelper {
      * Mirror an iterable. E.g.: 1,2,3,4 -> 1,2,3,4,4,3,2,1
      *
      * @param mirrorCenter if false do not duplicate center element
-     * @return the mirrored iterator
+     * @return the mirrored iterable
      */
     def mirror(mirrorCenter: Boolean = true): Iterable[A] =
       iterable.duplicate.reduce(_ ++ _.reverse.drop(if (mirrorCenter) 0 else 1)) to Iterable
 
+    /**
+     * Reverse an iterable. E.g.: 1,2,3,4 -> 4,3,2,1
+     *
+     * @return the reversed iterable
+     */
     def reverse: Iterable[A] = iterable.reverseIterator to Iterable
 
+    /**
+     * Get an optional random element from an iterable.
+     *
+     * @return None if the iterable is empty, a Some(element) otherwise
+     */
     def random: Option[A] = Option.when(iterable nonEmpty)(iterable(Random nextInt iterable.size))
   }
-
-  implicit def iteratorOf[A](iterable: Iterable[A]): Iterator[A] = iterable iterator
-
-  implicit def iterableOf[A](iterator: Iterator[A]): Iterable[A] = iterator to Iterable
-
-  implicit def sequenceOf[A](iterable: Iterable[A]): Seq[A] = iterable toSeq
-
-  implicit def sequenceOf[A](iterator: Iterator[A]): Seq[A] = iterator toSeq
 }

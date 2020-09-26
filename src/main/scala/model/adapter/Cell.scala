fa865bfd27a52c1a24d620b2538d7c573802d95b
Paolo Baldini <paolobaldini01@gmail.com>
2020-09-26 12:07:09 +0200

    added some scala docs

--- src/main/scala/model/adapter/Cell.scala
+++ src/main/scala/model/adapter/Cell.scala
@@ -1,10 +1,23 @@
 package scala.model.adapter
-
+// TODO move to environment to made old cell type package protected
+/**
+ * An object structure to share between environment and colonies
+ *
+ * @param temperature of the environment's cell in a numeric format
+ * @param humidity of the environment's cell in a numeric format
+ * @param pressure of the environment's cell in a numeric format
+ */
 case class Cell(temperature: Int = 0, humidity: Int = 0, pressure: Int = 0)
 
 object Cell {
   type OldCell = scala.model.environment.Cell
 
+  /**
+   * Convert an old-type cell to this one
+   *
+   * @param old type of cell
+   * @return this container cell
+   */
   implicit def toCell(old: OldCell): Cell = Cell(
     old.temperature.numericRepresentation(false),
     old.humidity.numericRepresentation(false),
--- src/main/scala/model/environment/matrix/Size.scala
+++ src/main/scala/model/environment/matrix/Size.scala
@@ -4,7 +4,7 @@ import scala.model.environment.matrix.Size.Border
 import scala.model.environment.matrix.Size.Border.Border
 
 /**
- * Represent the size of a 2d matrix
+ * Represent the size of a 2d element
  *
  * @author Paolo Baldini
  */
@@ -12,6 +12,12 @@ trait Size {
   val width: Int
   val height: Int
 
+  /**
+   * Get the 'distance' of the specified border from the center
+   *
+   * @param border to which calculate the distance
+   * @return the numeric distance of the border from the center
+   */
   def ~(border: Border): Int = border match {
     case Border.Top => - height / 2
     case Border.Bottom => height / 2
@@ -21,15 +27,33 @@ trait Size {
 }
 
 object Size {
+
+  /**
+   * Enumeration of the various border 'sides'
+   *
+   * @author Paolo Baldini
+   */
   object Border extends Enumeration {
     type Border = Value
     val Top, Right, Bottom, Left = Value
   }
 
+  /**
+   * Create a new 'stand-alone' size with specified width and height
+   *
+   * @param _width of the size
+   * @param _height of the size
+   * @return the size object
+   */
   def apply(_width: Int, _height: Int): Size = new Size {
     override val width: Int = _width
     override val height: Int = _height
   }
 
+  /**
+   * Implicitly convert a tuple to a Size
+   *
+   * @return the correspondent size of the tuple
+   */
   implicit def toSize: ((Int, Int)) => Size = p => Size(p._1, p._2)
 }

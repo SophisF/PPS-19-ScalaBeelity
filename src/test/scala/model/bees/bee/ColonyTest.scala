c5d308379738ad5d9b8c57df005b31934fe6d85a
Sophia <sophia.fantoni@studio.unibo.it>
2020-09-27 18:31:47 +0200

    Delete repetition of element in Colony

---
+++ src/test/scala/model/bees/bee/ColonyTest.scala
@@ -0,0 +1,38 @@
+package scala.model.bees.bee
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.model.bees.bee.Bee.Bee
+import scala.model.bees.bee.Colony.Colony
+import scala.model.bees.bee.Queen.Queen
+import scala.model.bees.genotype.Genotype
+import scala.model.bees.genotype.Genotype.Genotype
+import scala.util.Random
+import scala.utility.Point
+
+class ColonyTest extends AnyFunSuite {
+  val genotype: Genotype = Genotype()
+  var bees: Set[Bee] = (1 to 100).map(_ => Bee(genotype, genotype expressInPhenotype, 0, 20+Random.nextInt(20), 30+Random.nextInt(20), 950+Random.nextInt(100))).toSet
+  var queen: Queen = Queen(None, genotype, genotype expressInPhenotype, 0, 22, 1000, 45, Point(10,20), null)
+  val colony: Colony = queen.colony
+  val colony2: Colony = Colony(queen = queen, bees = bees)
+
+
+  test("Colony must have some Bees") {
+    assert(colony.bees.nonEmpty)
+  }
+
+  test("The initial number of worker bees must be == of reproduction rate + 1") {
+    assert(colony.bees.size == (queen.effectiveReproductionRate+2))
+  }
+
+  test("Dimension of initial colony must be proportionate at limit for cell of bee"){
+    assert(colony.area*5 >= colony.numberOfBees)
+  }
+
+  test("Dimension of colony must always be proportionate at limit for cell of bee"){
+    println("************" + colony2.bees.size)
+    assert(colony2.area*5 >= colony2.numberOfBees)
+  }
+
+}

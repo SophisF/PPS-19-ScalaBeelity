23493194a53407a623a92d118c440892ae73f7fa
Sophia <sophia.fantoni@studio.unibo.it>
2020-09-30 09:57:26 +0200

    Resolve warning in test

--- src/test/scala/utility/PointTest.scala
+++ src/test/scala/utility/PointTest.scala
@@ -10,14 +10,14 @@ class PointTest extends AnyFunSuite {
 
 
   test(s"$P  must have the coordinates, passed in apply function.") {
-    var result = Point apply(1, 2)
+    var result = Point.apply(1, 2)
 
     assert(result.x.equals(1) && result.y.equals(2))
   }
 
   test(s"$P with reassignment, must have the last coordinates.") {
-    var result = Point apply(1, 2)
-    result = Point apply(4,5)
+    var result = Point.apply(1, 2)
+    result = Point.apply(4,5)
 
     assert(result.x.equals(4) && result.y.equals(5))
   }
